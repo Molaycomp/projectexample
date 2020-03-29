62afb73dd0d09f5cfa2d5d072acbd911188fd04a
Molaycomp <62835174+Molaycomp@users.noreply.github.com>
2020-03-29 10:36:48 -0700

    code
    code

---
+++ b.java
@@ -0,0 +1,23 @@
+import java.sql.*;  
+class b{  
+public static void main(String args[]){  
+ try{  
+   //String database="college.mdb";
+   Class.forName("net.ucanaccess.jdbc.UcanaccessDriver"); 
+  
+   String url="jdbc:ucanaccess://college.mdb";  
+  
+  
+   Connection c=DriverManager.getConnection(url);  
+   Statement st=c.createStatement();  
+   ResultSet rs=st.executeQuery("select * from student");  
+    
+   while(rs.next()){  
+    System.out.println(rs.getString(1));  
+   }  
+  
+}catch(Exception ee){System.out.println(ee);}  
+  
+}}
+
+
